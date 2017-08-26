5788097e7381330f1dab3876bba7522460e2a4da
breakermind <hello@breakermind.com>
2017-08-26 15:03:15 +0100

    C++ ssl socket server multipleconnections openssl
    C++ ssl socket server multipleconnections openssl

--- SslServer/serverSsl.cpp
+++ SslServer/serverSsl.cpp
@@ -17,7 +17,7 @@ apt-get install openssl libssl-dev
 apt-get install libmysql++ libmysql++-dev libmysql++-doc
 
 Compile with: 
-g++ -o server server.cpp -lssl -lcrypto
+g++ -o server serverSsl.cpp -lssl -lcrypto
 
 Connect:
 openssl s_client -connect 127.0.0.1:3333
