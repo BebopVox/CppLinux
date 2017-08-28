4d6a99ba0501124c5d49ca4069a340b09e601cf1
breakermind <hello@breakermind.com>
2017-08-28 12:38:14 +0200

    Qt Json example
    Qt Json example

---
+++ QtJson/main.cpp
@@ -0,0 +1,40 @@
+#include <QCoreApplication>
+
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+
+#include <QString>
+#include <QDebug>
+
+QString json2 = QStringLiteral("{\"success\":true,\"properties\":[{\"ID\": 1001,\"PropertyName\":\"McDonalds\",\"key\":\"00112233445566778899aabbccddeeff\"},{\"ID\":1002,\"PropertyName\":\"Burger King\",\"key\":\"10112233445566778899aabbccddeeff\"},{\"ID\":1003,\"PropertyName\":\"Taco Bell\",\"key\":\"20112233445566778899aabbccddeeff\"}]}");
+
+void Json(QString json){
+
+    qDebug() << qPrintable(json);
+
+    QJsonDocument jsonResp = QJsonDocument::fromJson(json.toUtf8());
+    QJsonObject jsonObject = jsonResp.object();
+    QJsonArray jsonArray = jsonObject["properties"].toArray();
+
+    QStringList propertyNames;
+    QStringList propertyKeys;
+
+    foreach (const QJsonValue & value, jsonArray) {
+        QJsonObject obj = value.toObject();
+        propertyNames.append(obj["PropertyName"].toString());
+        propertyKeys.append(obj["key"].toString());
+        qDebug() << propertyNames;
+        qDebug() << propertyKeys;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    // Run json
+    Json(json2);
+
+    return a.exec();
+}
