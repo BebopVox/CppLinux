dd93d589d254d83acc540048e727115bc66d364e
breakermind <hello@breakermind.com>
2017-08-29 11:03:52 +0200

    Update breakermindsslserver.h

--- QtSslServerClass/breakermindsslserver.h
+++ QtSslServerClass/breakermindsslserver.h
@@ -9,29 +9,25 @@
 using namespace std;
 
 class BreakermindSslServer{
-
     public:
         BreakermindSslServer();
+        // Set ssl connection
+        SSL_CTX *create_context();
         int create_socket(int port);
         void ShowCerts(SSL* ssl);
         void init_openssl();
         void cleanup_openssl();
-        SSL_CTX *create_context();
         void configure_context(SSL_CTX *ctx, std::string Certificate, std::string CertificateKey);
         // Run server
         void Start(int Port, std::string Certificate, std::string CertificateKey);
-
         // Save to file
         void SaveToFile(const char* val);
 
-        std::string ServerCertificate = "";
-        std::string ServerKey = "";
-
     protected:
+        // Main loop send and receive data from clients here
+        void ServereLoop(SSL *ssl);
 
     private:
         SSL* ssl;
-
-
 };
 #endif // BREAKERMINDSSLSERVER_H
