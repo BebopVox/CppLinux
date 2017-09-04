7f685d7d766091ddf8396369fd276a1922aa8bb6
breakermind <hello@breakermind.com>
2017-09-04 16:49:18 +0200

    Create Breakermind.h

---
+++ MySqlConnector/MySqlConnectLibSsl/Breakermind.h
@@ -0,0 +1,12 @@
+#ifndef BREAKERMIND_H
+#define BREAKERMIND_H
+
+class Breakermind{
+	public:
+		Breakermind();
+		void SaveToFile(const char* val);
+	//protected:		
+	//private:
+};
+
+#endif // BREAKERMIND_H
