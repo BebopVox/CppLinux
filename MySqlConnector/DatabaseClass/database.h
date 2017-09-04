61ba2c354d54c158ec3d38a4d429a63025349905
breakermind <hello@breakermind.com>
2017-09-04 17:13:04 +0200

    Update database.h

--- MySqlConnector/DatabaseClass/database.h
+++ MySqlConnector/DatabaseClass/database.h
@@ -1,14 +1,36 @@
 #ifndef DATABASE_H
 #define DATABASE_H
 
+#include <stdlib.h>
+#include <iostream>
+
+#include "mysql_connection.h"
+#include "mysql_driver.h"
+#include "mysql_error.h"
+
+// usr/include/cppconn
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+#include <cppconn/prepared_statement.h>
+
 using namespace std;
 
 class Database
 {
+private:
+    sql::Driver *driver;
+    sql::Connection *con;
+    sql::Statement *stmt;
+    sql::ResultSet *res;
+    sql::PreparedStatement  *prep_stmt;
+
 public:
-    Database();
+    Database(string Hostname, string User, string Pass);
     void Connect();
     void InsertMsg();
+
 };
 
 #endif // DATABASE_H
