2b1a4165b34826ce999c2f93eb6a38b98b483b26
breakermind <hello@breakermind.com>
2017-09-04 16:33:35 +0200

    Create database.h

---
+++ MySqlConnector/DatabaseClass/database.h
@@ -0,0 +1,14 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+using namespace std;
+
+class Database
+{
+public:
+    Database();
+    void Connect();
+    void InsertMsg();
+};
+
+#endif // DATABASE_H
