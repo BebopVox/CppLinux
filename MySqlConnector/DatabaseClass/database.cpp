ff4985005e799474ff95da43cee7d47643053746
breakermind <hello@breakermind.com>
2017-09-04 17:30:09 +0200

    Update database.cpp

--- MySqlConnector/DatabaseClass/database.cpp
+++ MySqlConnector/DatabaseClass/database.cpp
@@ -1,10 +1,10 @@
 #include "database.h"
 
-Database::Database(string Hostname, string User, string Pass)
+Database::Database(string Hostname, string Port, string User, string Pass)
 {
     /* Create a connection */
     driver = get_driver_instance();
-    con = driver->connect("tcp://"+Hostname+":3306", User, Pass);
+    con = driver->connect("tcp://"+Hostname+":"+Port, User, Pass);
     /* Connect to the MySQL database */
     con->setSchema("BreakermindSMTP");
 }
@@ -48,18 +48,34 @@ void Database::InsertMsg(){
         prep_stmt->setString(1, "benku");
         prep_stmt->execute();
 
-        stmt = con->createStatement();
-        // res = stmt->executeQuery("SELECT @@identity AS id");
-        res = stmt->executeQuery("SELECT LAST_INSERT_ID() as id");
-        res->next();
-        unsigned long long retVal = res->getInt64("id");
-        cout << "!!!!!!!!!! Last id: " << retVal;
+        unsigned long long id = LastInsertID();
+        cout << "!!!!!!!!!! Last id: " << id << endl;
+
     } catch (sql::SQLException &e) {
       cout << "# ERR: SQLException in " << __FILE__;
       cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
       cout << "# ERR: " << e.what();
       cout << " (MySQL error code: " << e.getErrorCode();
       cout << ", SQLState: " << e.getSQLState() << " )" << endl;
+    }
+}
+
+unsigned long long Database::LastInsertID(){
+    try{
+        stmt = con->createStatement();
+        // res = stmt->executeQuery("SELECT @@identity AS id");
+        res = stmt->executeQuery("SELECT LAST_INSERT_ID() as id");
+        res->next();
+        unsigned long long retVal = res->getInt64("id");
+        // cout << "!!!!!!!!!! Last id: " << retVal;
+        return retVal;
+    } catch (sql::SQLException &e) {
+      cout << "# ERR: SQLException in " << __FILE__;
+      cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
+      cout << "# ERR: " << e.what();
+      cout << " (MySQL error code: " << e.getErrorCode();
+      cout << ", SQLState: " << e.getSQLState() << " )" << endl;
+      return 0;
     }
 }
 
