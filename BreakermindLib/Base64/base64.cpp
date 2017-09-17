40998e14412811ce4bd585322805fd8a58b25e03
breakermind <hello@breakermind.com>
2017-09-17 18:34:14 +0200

    C++ base64
    C++ base64

--- BreakermindLib/Base64/base64.cpp
+++ BreakermindLib/Base64/base64.cpp
@@ -84,8 +84,49 @@ std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_
 
 }
 
-std::string base64_encode(std::string str, unsigned int in_len) {
+std::string base64_encode(std::string str, unsigned int in_len) {  
+  unsigned char const* bytes_to_encode = reinterpret_cast<const unsigned char*>(str.c_str());
+  std::string ret;
+  int i = 0;
+  int j = 0;
+  unsigned char char_array_3[3];
+  unsigned char char_array_4[4];
 
+  while (in_len--) {
+    char_array_3[i++] = *(bytes_to_encode++);
+    if (i == 3) {
+      char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+      char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+      char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+      char_array_4[3] = char_array_3[2] & 0x3f;
+
+      for(i = 0; (i <4) ; i++)
+        ret += base64_chars[char_array_4[i]];
+      i = 0;
+    }
+  }
+
+  if (i)
+  {
+    for(j = i; j < 3; j++)
+      char_array_3[j] = '\0';
+
+    char_array_4[0] = ( char_array_3[0] & 0xfc) >> 2;
+    char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+    char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+
+    for (j = 0; (j < i + 1); j++)
+      ret += base64_chars[char_array_4[j]];
+
+    while((i++ < 3))
+      ret += '=';
+
+  }
+  return ret;
+}
+
+std::string base64_encode(std::string str) {
+  unsigned int in_len = str.length();
   unsigned char const* bytes_to_encode = reinterpret_cast<const unsigned char*>(str.c_str());
   std::string ret;
   int i = 0;
@@ -127,6 +168,7 @@ std::string base64_encode(std::string str, unsigned int in_len) {
   return ret;
 
 }
+
 std::string base64_decode(std::string const& encoded_string) {
   int in_len = encoded_string.size();
   int i = 0;
