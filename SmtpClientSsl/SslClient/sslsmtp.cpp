eb23a71d124ab7e3bd997c153d347b4f7eb3226d
breakermind <hello@breakermind.com>
2017-09-17 22:30:14 +0200

    C++ ssl smpt socket client
     C++ ssl smpt socket client

--- SmtpClientSsl/SslClient/sslsmtp.cpp
+++ SmtpClientSsl/SslClient/sslsmtp.cpp
@@ -1,7 +1,106 @@
 #include "sslsmtp.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+#include <cstdio>
+#include <algorithm>
 
 // BreakermindLib base64
 #include "BreakermindLib/breakermind.h"
+
+
+const char SslSMTP::MimeTypes[][2][128] =
+{
+    {"***",    "application/octet-stream"},
+    {"csv",    "text/csv"},
+    {"tsv",    "text/tab-separated-values"},
+    {"tab",    "text/tab-separated-values"},
+    {"html",    "text/html"},
+    {"htm",    "text/html"},
+    {"doc",    "application/msword"},
+    {"docx",    "application/vnd.openxmlformats-officedocument.wordprocessingml.document"},
+    {"ods",    "application/x-vnd.oasis.opendocument.spreadsheet"},
+    {"odt",    "application/vnd.oasis.opendocument.text"},
+    {"rtf",    "application/rtf"},
+    {"sxw",    "application/vnd.sun.xml.writer"},
+    {"txt",    "text/plain"},
+    {"xls",    "application/vnd.ms-excel"},
+    {"xlsx",    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"},
+    {"pdf",    "application/pdf"},
+    {"ppt",    "application/vnd.ms-powerpoint"},
+    {"pps",    "application/vnd.ms-powerpoint"},
+    {"pptx",    "application/vnd.openxmlformats-officedocument.presentationml.presentation"},
+    {"wmf",    "image/x-wmf"},
+    {"atom",    "application/atom+xml"},
+    {"xml",    "application/xml"},
+    {"json",    "application/json"},
+    {"js",    "application/javascript"},
+    {"ogg",    "application/ogg"},
+    {"ps",    "application/postscript"},
+    {"woff",    "application/x-woff"},
+    {"xhtml","application/xhtml+xml"},
+    {"xht",    "application/xhtml+xml"},
+    {"zip",    "application/zip"},
+    {"gz",    "application/x-gzip"},
+    {"rar",    "application/rar"},
+    {"rm",    "application/vnd.rn-realmedia"},
+    {"rmvb",    "application/vnd.rn-realmedia-vbr"},
+    {"swf",    "application/x-shockwave-flash"},
+    {"au",        "audio/basic"},
+    {"snd",    "audio/basic"},
+    {"mid",    "audio/mid"},
+    {"rmi",        "audio/mid"},
+    {"mp3",    "audio/mpeg"},
+    {"aif",    "audio/x-aiff"},
+    {"aifc",    "audio/x-aiff"},
+    {"aiff",    "audio/x-aiff"},
+    {"m3u",    "audio/x-mpegurl"},
+    {"ra",    "audio/vnd.rn-realaudio"},
+    {"ram",    "audio/vnd.rn-realaudio"},
+    {"wav",    "audio/x-wave"},
+    {"wma",    "audio/x-ms-wma"},
+    {"m4a",    "audio/x-m4a"},
+    {"bmp",    "image/bmp"},
+    {"gif",    "image/gif"},
+    {"jpe",    "image/jpeg"},
+    {"jpeg",    "image/jpeg"},
+    {"jpg",    "image/jpeg"},
+    {"jfif",    "image/jpeg"},
+    {"png",    "image/png"},
+    {"svg",    "image/svg+xml"},
+    {"tif",    "image/tiff"},
+    {"tiff",    "image/tiff"},
+    {"ico",    "image/vnd.microsoft.icon"},
+    {"css",    "text/css"},
+    {"bas",    "text/plain"},
+    {"c",        "text/plain"},
+    {"h",        "text/plain"},
+    {"rtx",    "text/richtext"},
+    {"mp2",    "video/mpeg"},
+    {"mpa",    "video/mpeg"},
+    {"mpe",    "video/mpeg"},
+    {"mpeg",    "video/mpeg"},
+    {"mpg",    "video/mpeg"},
+    {"mpv2",    "video/mpeg"},
+    {"mov",    "video/quicktime"},
+    {"qt",    "video/quicktime"},
+    {"lsf",    "video/x-la-asf"},
+    {"lsx",    "video/x-la-asf"},
+    {"asf",    "video/x-ms-asf"},
+    {"asr",    "video/x-ms-asf"},
+    {"asx",    "video/x-ms-asf"},
+    {"avi",    "video/x-msvideo"},
+    {"3gp",    "video/3gpp"},
+    {"3gpp",    "video/3gpp"},
+    {"3g2",    "video/3gpp2"},
+    {"movie","video/x-sgi-movie"},
+    {"mp4",    "video/mp4"},
+    {"wmv",    "video/x-ms-wmv"},
+    {"webm","video/webm"},
+    {"m4v",    "video/x-m4v"},
+    {"flv",    "video/x-flv"}
+};
 
 SslSMTP::SslSMTP(int port = 0)
 {
@@ -95,11 +194,14 @@ bool SslSMTP::Send(string hostnameMX, string from, string to, string replyto, st
                     std::string path = files.at(i);
                     std::string filename = fileBasename(path);
                     std::string fc = base64_encode(get_file_contents(filename.c_str()));
+                    std::string extension = GetFileExtension(filename);
+                    const char *mimetype = GetMimeTypeFromFileName((char*)extension.c_str());
+                    cout << "MIME " << mimetype << endl << extension << endl;
 
                     // cout << "FILE CONTENT " << fc << endl;
 
                     m << "--ToJestSeparator0000\r\n";
-                    m << "Content-Type: image/jpeg; name=\"" << filename << "\"\r\n";
+                    m << "Content-Type: " << mimetype << "; name=\"" << filename << "\"\r\n";
                     m << "Content-Transfer-Encoding: base64\r\n";
                     m << "Content-Disposition: attachment; filename=\"" << filename << "\"\r\n\r\n";
                     m << fc <<"\r\n\r\n";
@@ -263,6 +365,29 @@ std::string SslSMTP::get_file_contents(const char *filename)
   }
   throw(errno);
 }
+
+
+std::string SslSMTP::GetFileExtension(const std::string& FileName)
+{
+    if(FileName.find_last_of(".") != std::string::npos)
+        return FileName.substr(FileName.find_last_of(".")+1);
+    return "";
+}
+
+const char* SslSMTP::GetMimeTypeFromFileName( char* szFileExt)
+{
+    cout << "EXT " << szFileExt;
+
+    for (int i = 0; i < sizeof (MimeTypes) / sizeof (MimeTypes[0]); i++)
+    {
+        if (strcmp(MimeTypes[i][0],szFileExt) == 0)
+        {
+            return MimeTypes[i][1];
+        }
+    }
+    return MimeTypes[0][1];   //if does not match any,  "application/octet-stream" is returned
+}
+
 
 /*
  char *mdata = (char*)"From: <hello@qflash.pl>\r\n"
