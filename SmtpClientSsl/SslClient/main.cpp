ce4b071b387d51d543eb49a760d6fe136313c810
breakermind <hello@breakermind.com>
2017-09-18 09:39:36 +0200

    C++ ssl smpt socket client
    C++ ssl smpt socket client

--- SmtpClientSsl/SslClient/main.cpp
+++ SmtpClientSsl/SslClient/main.cpp
@@ -64,9 +64,9 @@ int main(int count, char *strings[])
     SslSMTP smtp = SslSMTP(25);
     vector<string> files;
     files.push_back("file9.jpg");
-    files.push_back("file8.jpg");
+    files.push_back("filek.pdf");
     // send email
-    smtp.Send("qflash.pl", "Breakemrind <hello@breakermind.com>", "<hello@qflash.pl>", "<hello@breakermind.com>", "Hello111111=2C_czy_um=F3wisz_si=EA_ze_mn=B1=3F?=", "WIadomosć tekstowa dla usera", "<h1>Hello from html msg.</h1>", files);
+    smtp.Send("qflash.pl", "<hello@breakermind.com>", "<info@qflash.pl>", "<hello@breakermind.com>", "Hello Zombie=2C_czy_um=F3wisz_si=EA_ze_mn=B1=3F?=", "WIadomosć tekstowa dla usera", "<h1>Hello from html msg.</h1>", files);
 
     cout << "\r\nStop smtp";
 
