cc8e3895f98f2c5c01993c85ece7d198c984dff7
breakermind <hello@breakermind.com>
2017-09-19 12:20:24 +0200

    c++ ssl socket telnet strange socket data
    c++ ssl socket telnet strange socket data

--- SslSocketTelnetRubish.cpp
+++ SslSocketTelnetRubish.cpp
@@ -14,5 +14,16 @@ received = SSL_read (ssl, buffer, readSize);
 if (buffer[received-1] == '\n') buffer[received-1] = '\0';
 // use buffer
 json1 = std::string(buffer);
+// cut < and >        
+// json1 = replaceAll(json1, "<", "");
+std::cout << json1 << endl;
+// json1 = replaceAll(json1, ">", "");        
+std::cout << json1 << endl;
+json1 = replaceAll(json1, "\r", "");
+std::cout << json1 << endl;
+json1 = replaceAll(json1, "\n", "");
+std::cout << json1 << endl;
+json1 = replaceAll(json1, " ", "");
+std::cout << json1 << endl;
 // clear buffer
 strcpy(buffer, "");
