5f0ef6360f5d08e6ec80b1d461c56d939a80e2fe
breakermind <hello@breakermind.com>
2017-10-24 19:10:30 +0100

    C++ html entities encode, decode
    C++ html entities encode, decode (mysql sql injection or display html)

---
+++ HtmlEntities/htmlentities.cpp
@@ -0,0 +1,80 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+string entitiesReplace(string str) {
+	string subs[] = {"&semi;", "&amp;", "&quot;", "&apos;", "&lt;", "&gt;", "&colon;", "&equals;", "&excl;"};	
+	string reps[] = {"#semi#", "#amp#", "#quot#", "#apos#", "#lt#", "#gt#", "#colon#", "#equals#", "#excl#"};	
+	size_t found;
+	for(int j = 0; j < 9; j++) {
+		do {
+			found = str.find(subs[j]);
+	  		if (found != string::npos){
+		    	str.replace(found,subs[j].length(),reps[j]);
+		    }
+    	} while (found != string::npos);
+  	}
+	return str;
+}
+
+string entitiesReplaceBack(string str) {
+	string reps[] = {"&semi;", "&amp;", "&quot;", "&apos;", "&lt;", "&gt;", "&colon;", "&equals;", "&excl;"};	
+	string subs[] = {"#semi#", "#amp#", "#quot#", "#apos#", "#lt#", "#gt#", "#colon#", "#equals#", "#excl#"};	
+	size_t found;
+	for(int j = 0; j < 9; j++) {
+		do {
+			found = str.find(subs[j]);
+	  		if (found != string::npos){
+		    	str.replace(found,subs[j].length(),reps[j]);
+		    }
+    	} while (found != string::npos);
+  	}
+	return str;
+}
+
+string entitiesDecode (string str) {	
+	string subs[] = {"&semi;", "&amp;", "&quot;", "&apos;", "&lt;", "&gt;", "&colon;", "&equals;", "&excl;"};	
+	string reps[] = {";", "&", "\"", "'", "<", ">", ":", "=", "!"};	
+	size_t found;
+	for(int j = 0; j < 9; j++) {
+		do {
+			found = str.find(subs[j]);
+	  		if (found != string::npos){
+		    	str.replace(found,subs[j].length(),reps[j]);
+		    }
+    	} while (found != string::npos);
+  	}
+  	str = entitiesReplaceBack(str);
+	return str;
+}
+
+string entitiesEncode (string str) {
+	str = entitiesReplace(str);
+	string subs[] = {";", "&", "\"", "'", "<", ">", ":", "=", "!"};	
+	string reps[] = {"&semi;", "&amp;", "&quot;", "&apos;", "&lt;", "&gt;", "&colon;", "&equals;", "&excl;"};
+	size_t found;
+	for(int j = 0; j < 9; j++) {
+		do {
+			found = str.find(subs[j]);
+	  		if (found != string::npos){
+		    	str.replace(found,subs[j].length(),reps[j]);
+		    }
+    	} while (found != string::npos);
+  	}
+	return str;
+}
+
+int main(){
+	
+	std::string html = "<h1>Hello &quot; girls </h1> 'asssdad'! No i teraz jakiś \"CUDZYSLOWIE\". Nowe Ju&amp; zdanie.";
+	
+	// Encode entities for mysql
+	string encode = entitiesEncode(html);	
+	cout << "Encode: " << encode << endl << endl;
+
+	// Decode
+	string decode = entitiesDecode(encode);
+	cout << "Decode : " << decode << endl << endl;
+	
+}
