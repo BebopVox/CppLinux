768fe0093bb5d0d07e880c758ddd8193e1a123bc
breakermind <hello@breakermind.com>
2017-11-21 19:22:36 +0100

    Php license script
    Php license script

---
+++ LibCurlLicense/index.php
@@ -0,0 +1,16 @@
+<?php
+
+// Get app id
+// $app = (int)$_GET['app'];
+
+// Get key value
+// $key = htmlentities($_GET['key'],ENT_QUOTES,'UTF-8');
+
+// if key valid and app = 1
+if($key == "ID-999" && $app == 1){
+  return 1;
+}else{
+  return 0;
+}
+
+?>
